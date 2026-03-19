613d52cb9bf353c66ef21bb3a6f259fe7a5dc940
vinsonvu <77460458+vinsonvu@users.noreply.github.com>
2026-03-18 22:49:13 -0700

    Add query for accidents at stop signs vs roundabouts
    Added a query to compare accidents at stop signs and roundabouts.

--- Vu, Vinson_U.S.Accidents.sql
+++ Vu, Vinson_U.S.Accidents.sql
@@ -15,4 +15,12 @@ GROUP By w.Weather_Condition
 ORDER BY Total_Accidents DESC
 LIMIT 5;
 
--- Simple Query 2 - Draw insights
+-- Simple Query 2 - Draw insights comparing accidents at stop signs versus roundabouts
+SELECT
+    r.Stop_Sign,
+    r.Roundabout,
+    COUNT(a.ID) AS Total_Accidents
+FROM accidents a
+JOIN road r ON a.Road_ID = r.Road_ID
+WHERE r.Stop_Sign = TRUE OR r.Roundabout = TRUE
+GROUP BY r.Stop_Sign, r.Roundabout;
